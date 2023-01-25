fa428efe88368f4c1aa4b6191059fb1ce6f76529
Abdi Berhe <abdi201380@gmail.com>
2023-01-24 18:26:27 -0800

    Adding tested files of printf

--- print_functions1.c
+++ print_functions1.c
@@ -1,92 +1,185 @@
 #include "main.h"
-#include <stdarg.h>
-#include <stdlib.h>
 
 /**
- * print_unsignedToBinary - prints an integer.
- * @arg: argument
- * Return: 0
+ * print_char - Prints a char
+ * @types: List a of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: Width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
  */
-int print_unsignedToBinary(va_list arg)
+int print_char(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
 {
+	char c = va_arg(types, int);
 
-unsigned int n = va_arg(arg, unsigned int);
-unsigned int printed;
-
-print_binary(n, &printed);
-print_binary(n, &printed);
-
-return (printed);
-}
-
-
-/**
- * print_oct - prints number in octal base.
- * @arg: list containing octal number to be printed
- * Return: number of octals printed
- */
-
-int print_oct(va_list arg)
-{
-	unsigned int num = va_arg(arg, unsigned int);
-	unsigned int copy;
-	char *octa;
-	int i, j, charPrinted = 0;
-
-	if (num == 0)
-		return (_putchar('0'));
-	for (copy = num; copy != 0; j++)
-	{
-		copy = copy / 8;
-	}
-	octa = malloc(j);
-	if (!octa)
-		return (-1);
-
-	for (i = j - 1; i >= 0; i--)
-	{
-		octa[i] = num % 8 + '0';
-		num = num / 8;
-	}
-
-	for (i = 0; i < j && octa[i] == '0'; i++)
-		;
-	for (; i < j; i++)
-	{
-		_putchar(octa[i]);
-		charPrinted++;
-	}
-	free(octa);
-	return (charPrinted);
+	return (handle_write_char(c, buffer, flags, width, precision, size));
 }
 
 /**
- * print_unsignedIntToHex - prints unsigned int to hexadecimal.
- * @num: number to print
- * @_case: letter `a` on the case to print it (upper or lower)
- * Return: number or char printed
+ * print_string - Prints a string
+ * @types: List a of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
  */
-int print_unsignedIntToHex(unsigned int num, char _case)
+int print_string(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
 {
-	unsigned int num2;
-	int i, j, remainder, nbrCharacters = 0;
-	char *numhex;
+	int length = 0, i;
+	char *str = va_arg(types, char *);
 
-	for (num2 = num; num2 != 0; nbrCharacters++, num2 /= 16)
-	;
-
-	numhex = malloc(nbrCharacters);
-	for (i = 0; num != 0; i++)
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+	if (str == NULL)
 	{
-		remainder = num % 16;
-		if (remainder < 10)
-			numhex[i] = remainder + '0';
+		str = "(null)";
+		if (precision >= 6)
+			str = "      ";
+	}
+
+	while (str[length] != '\0')
+		length++;
+
+	if (precision >= 0 && precision < length)
+		length = precision;
+
+	if (width > length)
+	{
+		if (flags & F_MINUS)
+		{
+			write(1, &str[0], length);
+			for (i = width - length; i > 0; i--)
+				write(1, " ", 1);
+			return (width);
+		}
 		else
-			numhex[i] = remainder - 10 + _case;
-		num = num / 16;
+		{
+			for (i = width - length; i > 0; i--)
+				write(1, " ", 1);
+			write(1, &str[0], length);
+			return (width);
+		}
 	}
-	for (j = i - 1; j >= 0; j--)
-		_putchar(numhex[j]);
-	free(numhex);
-	return (nbrCharacters);
+
+	return (write(1, str, length));
 }
+
+/**
+ * print_percent - Prints a percent sign
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+int print_percent(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	UNUSED(types);
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+	return (write(1, "%%", 1));
+}
+
+/**
+ * print_int - Print int
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+int print_int(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	int i = BUFF_SIZE - 2;
+	int is_negative = 0;
+	long int n = va_arg(types, long int);
+	unsigned long int num;
+
+	n = convert_size_number(n, size);
+
+	if (n == 0)
+		buffer[i--] = '0';
+
+	buffer[BUFF_SIZE - 1] = '\0';
+	num = (unsigned long int)n;
+
+	if (n < 0)
+	{
+		num = (unsigned long int)((-1) * n);
+		is_negative = 1;
+	}
+
+	while (num > 0)
+	{
+		buffer[i--] = (num % 10) + '0';
+		num /= 10;
+	}
+
+	i++;
+
+	return (write_number(is_negative, i, buffer, flags, width, precision, size));
+}
+
+/**
+ * print_binary - Prints an unsigned number
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Numbers of char printed.
+ */
+int print_binary(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	unsigned int n, m, i, sum;
+	unsigned int a[32];
+	int count;
+
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+	n = va_arg(types, unsigned int);
+	m = 2147483648; /* (2 ^ 31) */
+	a[0] = n / m;
+	for (i = 1; i < 32; i++)
+	{
+		m /= 2;
+		a[i] = (n / m) % 2;
+	}
+	for (i = 0, sum = 0, count = 0; i < 32; i++)
+	{
+		sum += a[i];
+		if (sum || i == 31)
+		{
+			char z = '0' + a[i];
+
+			write(1, &z, 1);
+			count++;
+		}
+	}
+	return (count);
+}
+
